2fe985afb3db729ba3d2bfa5675ce39cf0cd86b3
Gabryel henrique sousa da silva <80103175+projetos-Gabryel@users.noreply.github.com>
2022-02-17 09:00:41 -0300

    Add files via upload

---
+++ aula_01/index.php
@@ -0,0 +1,31 @@
+
+ <?php
+     //isso é um comentário de 1 linha
+     /*dbhsdhgourshuhuudashfuioh
+       hfuassgfgusahfuaysdufyuay
+       */
+       
+       $nome = "gabryel";
+       $idade = 17;
+       $trabalhando= TRUE;
+       echo "Meu nome é ",$nome ;
+       echo "<br>";
+       echo $idade;
+       echo "<br>";
+       echo $trabalhando;
+       echo "<br>";
+       $numero = '12';
+
+       echo $idade.$numero;
+       echo "<br>";
+
+       var_dump($numero) ;
+       
+
+
+
+
+?>
+
+
+ 
